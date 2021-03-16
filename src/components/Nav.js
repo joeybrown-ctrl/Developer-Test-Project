594f5341f01e0d776ca3f6e97c21e677fe7ad6b2
Joey Brown <gjoey.brown@gmail.com>
2021-03-16 15:39:55 -0700

    fixed Nav component links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,20 +6,12 @@ function Nav() {
         <>
             <nav className="navbar navbar-light bg-dark fixed-top">
                 <div className="container-fluid">
+                    
                     <a className="navbar-brand text-light" href="/">Developer-Test-Store</a>
-
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Dropdown
-                            </a>
-                            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a className="dropdown-item" href="/">Products</a></li>
-                                <li><a className="dropdown-item" href="/categories">Categories</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-
+                    <div className="row justify-content-right">
+                        <a className="navbar-brand text-light" href="/">Products</a>
+                        <a className="navbar-brand text-light" href="/categories">Categories</a>
+                    </div>
                 </div>
             </nav>
 
